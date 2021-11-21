956db4ef58b0944a9906c1be1262078a98a82183
Prizm Moon <prdigitalmoon@gmail.com>
2021-11-21 07:26:38 -0500

    Fixes "salamander" in query to be "salamanders"

--- private/query_functions.php
+++ private/query_functions.php
@@ -2,7 +2,7 @@
 
   function find_all_salamanders() {
     global $db;
-    $sql = "SELECT * FROM salamander ";
+    $sql = "SELECT * FROM salamanders ";
     $sql .= "ORDER BY name ASC";
     $result = mysqli_query($db, $sql);
     return $result;
