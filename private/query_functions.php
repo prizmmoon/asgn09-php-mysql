6a3a1814146eca571d1595d3005430f228e4cafd
Prizm Moon <prdigitalmoon@gmail.com>
2021-11-21 07:21:39 -0500

    Fixes an = to be .= in query function

--- private/query_functions.php
+++ private/query_functions.php
@@ -3,7 +3,7 @@
   function find_all_salamanders() {
     global $db;
     $sql = "SELECT * FROM salamander ";
-    $sql = "ORDER BY name ASC";
+    $sql .= "ORDER BY name ASC";
     $result = mysqli_query($db, $sql);
     return $result;
   }  
