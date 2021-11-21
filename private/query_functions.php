dc204f96c0ff51a7054e3c8a2b4355e3d53be5b8
Prizm Moon <prdigitalmoon@gmail.com>
2021-11-21 06:52:54 -0500

    Pastes in database, query functions files

---
+++ private/query_functions.php
@@ -0,0 +1,28 @@
+<?php
+
+  function find_all_subjects() {
+    global $db;
+
+    $sql = "SELECT * FROM subjects ";
+    $sql .= "ORDER BY position ASC";
+    $result = mysqli_query($db, $sql);
+
+    //echo $sql;
+
+    confirm_result_set($result);
+    return $result;
+  }
+
+  function find_all_pages() {
+    global $db;
+
+    $sql = "SELECT * FROM pages ";
+    $sql .= "ORDER BY subject_id ASC, position ASC";
+    $result = mysqli_query($db, $sql);
+
+    //echo $sql;
+
+    confirm_result_set($result);
+    return $result;
+  }
+?>
