56b4b8d4c65affa8b9ede49fa77bf35d6d42415a
Prizm Moon <prdigitalmoon@gmail.com>
2021-11-21 07:04:33 -0500

    Replaces query functions with salamander function

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,28 +1,11 @@
 <?php
 
-  function find_all_subjects() {
+  function find_all_salamanders() {
     global $db;
-
-    $sql = "SELECT * FROM subjects ";
-    $sql .= "ORDER BY position ASC";
+    $sql = "SELECT * FROM salamander ";
+    $sql = "ORDER BY name ASC";
     $result = mysqli_query($db, $sql);
-
-    //echo $sql;
-
-    confirm_result_set($result);
     return $result;
-  }
+  }  
 
-  function find_all_pages() {
-    global $db;
-
-    $sql = "SELECT * FROM pages ";
-    $sql .= "ORDER BY subject_id ASC, position ASC";
-    $result = mysqli_query($db, $sql);
-
-    //echo $sql;
-
-    confirm_result_set($result);
-    return $result;
-  }
 ?>
