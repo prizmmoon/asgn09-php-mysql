f7596dc0940698cb392ac9c61fcc99352417fdf0
Prizm Moon <prdigitalmoon@gmail.com>
2021-11-23 14:52:40 -0500

    Fixed query to use salamander database

--- private/query_functions.php
+++ private/query_functions.php
@@ -2,7 +2,7 @@
 
   function find_all_salamanders() {
     global $db;
-    $sql = "SELECT * FROM salamanders ";
+    $sql = "SELECT * FROM salamander ";
     $sql .= "ORDER BY name ASC";
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
