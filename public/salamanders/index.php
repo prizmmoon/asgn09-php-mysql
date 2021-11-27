0ae392fd7ede40bd62ad821dde9642b87bb50414
Prizm Moon <prdigitalmoon@gmail.com>
2021-11-26 19:01:49 -0500

    Fixes table to use correct fields and field types

--- public/salamanders/index.php
+++ public/salamanders/index.php
@@ -28,9 +28,9 @@
       <?php while($salamander = mysqli_fetch_assoc($salamander_set)) { ?>
         <tr>
           <td><?= h($salamander['id']); ?></td>
-          <td><?= h($salamander['position']); ?></td>
-          <td><?= $salamander['visible'] == 1 ? 'true' : 'false'; ?></td>
-    	    <td><?= h($salamander['salamanderName']); ?></td>
+          <td><?= h($salamander['name']); ?></td>
+          <td><?= h($salamander['habitat']); ?></td>
+    	    <td><?= h($salamander['description']); ?></td>
           <td><a class="action" href="<?= url_for('salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
           <td><a class="action" href="<?= url_for('salamanders/edit.php?id=' . h(u($salamander['id']))); ?>">Edit</a></td>
           <td><a class="action" href="">Delete</a></td>
